498baefac5ebb9a2f1032f987f8b28b9576b86ac
GuoYancheng <guoyancheng@fang.com>
2020-10-09 18:11:30 +0800

    【新增】Leet1_TwoSum

---
+++ src/Leet1_TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *
+ */
+public class Leet1_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        //hash法
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            int result = target - nums[i];
+            if (map.containsKey(result)) {
+                return new int[]{map.get(result), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
