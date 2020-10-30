0bab57239e63ba0fb7557083f7fb195a04f9981e
GuoYancheng <guoyancheng@fang.com>
2020-10-30 18:51:08 +0800

    【新增】Leet53_maxSubArray Leet118_PascalTriangle

---
+++ src/dp/Leet118_PascalTriangle.java
@@ -0,0 +1,39 @@
+package dp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * <p>
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 5
+ * 输出:
+ * [
+ * [1],
+ * [1,1],
+ * [1,2,1],
+ * [1,3,3,1],
+ * [1,4,6,4,1]
+ * ]
+ */
+public class Leet118_PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        ArrayList<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0 || j == i) {
+                    temp.add(1);
+                } else {
+                    temp.add(result.get(i - 1).get(j) + result.get(i - 1).get(j - 1));
+                }
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+}
